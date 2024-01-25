3e96404b1f0c95dbcc1906e0b4ba042e7f65af78
sknsecdeveloper <subhash.nsec@gmail.com>
2024-01-25 11:23:28 +0530

    commint2

---
+++ src/arnab/diamond/NinjaInterface.java
@@ -0,0 +1,30 @@
+package arnab.diamond;
+
+public interface NinjaInterface {
+
+    final int x = 100;
+
+    // Publicly accessible abstract.
+    void NinjaDisplay();
+}
+
+class NinjaClass implements NinjaInterface
+{
+    // Defining the function definition.
+    public void NinjaDisplay()
+    {
+        System.out.println("Welcome Ninjas!!");
+    }
+
+    // Main function
+    public static void main(String[] args)
+    {
+        NinjaClass obj = new NinjaClass();
+
+        // Calling NinjaDisplay.
+        obj.NinjaDisplay();
+
+        // Accessing public variable of NinjaInterface.
+        System.out.println(x);
+    }
+}
