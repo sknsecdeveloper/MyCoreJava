1762e72d53c9940fe7c360047d00bb122c661850
sknsecdeveloper <subhash.nsec@gmail.com>
2024-02-12 09:53:29 +0530

    filter
    filter

--- src/com/arnab/interview/TestFilterOptional.java
+++ src/com/arnab/interview/TestFilterOptional.java
@@ -1,8 +1,15 @@
 package com.arnab.interview;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class TestFilterOptional {
 
     public static void main(String[] args) {
+
+        int[] nums = {1,4,6,3,6,3,0,1};
+
+        Arrays.stream(nums).boxed().filter(n-> n%2==0).collect(Collectors.toList()).forEach(System.out::println);
 
     }
 }
